2bfbc69bf565ccccac87c7f2e63b31a02ec2e9b5
Dharang Sharma <dharangsharma@yahoo.in>
2020-03-13 19:01:32 +0530

    Add inital dice game website

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 =Math.floor(Math.random()*6)+1;
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomDiceSource = "images/"+randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceSource);
+
+
+var randomNumber2 =Math.floor(Math.random()*6)+1;
+var randomDiceImage = "dice"+randomNumber2+".png";
+var randomDiceSource2 = "images/"+randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceSource2);
+
+if(randomNumber1>randomNumber2){
+var player1Won = document.querySelector("h1").innerHTML = "&#x1f6a9 Player 1 Won";
+}
+else if (randomNumber2>randomNumber1) {
+  var player1Won = document.querySelector("h1").innerHTML = "&#x1f6a9 Player 2 Won";
+}
+else{
+  var draw = document.querySelector("h1").innerHTML = "Draw";
+}
